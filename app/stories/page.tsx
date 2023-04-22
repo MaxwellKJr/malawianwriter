432ba23d14ec8b3861b2808d47066ea79dee1dc2
maxwellkjr <maxwellkjr@gmail.com>
2023-04-22 23:14:33 +0200

    FIXES on Card Components for Blogs and Stories

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,45 +1,45 @@
-import { Metadata } from 'next';
-import Stories from '@/components/Stories/Stories';
-import PageWrapper from '@/components/PageWrapper';
-import getStoriesMetadata from '@/components/models/getStoryMetadata';
+import { Metadata } from "next";
+import Stories from "@/components/Stories/Stories";
+import PageWrapper from "@/components/PageWrapper";
+import getStoriesMetadata from "@/components/models/getStoryMetadata";
 
 export const metadata: Metadata = {
-	title: 'Malawian Writer 🇲🇼 - Stories',
-	description:
-		'The Malawian Writer fiends for great stories, books and writing. From African Literature to stories from "A Galaxy Far Far Away".',
-	openGraph: {
-		title: 'Malawian Writer on Writing 🇲🇼 | Blog',
-		description:
-			'The Malawian Writer shares everything regarding writing and storytelling in generally, favorite books, recommendations and in search of the next GREAT story.',
-		url: 'https://malawianwriter.vercel.app',
-		siteName: 'Malawian Writer',
-		type: 'website',
-	},
+  title: "Malawian Writer 🇲🇼 - Stories",
+  description:
+    'The Malawian Writer fiends for great stories, books and writing. From African Literature to stories from "A Galaxy Far Far Away".',
+  openGraph: {
+    title: "Malawian Writer on Writing 🇲🇼 | Blog",
+    description:
+      "The Malawian Writer shares everything regarding writing and storytelling in generally, favorite books, recommendations and in search of the next GREAT story.",
+    url: "https://malawianwriter.vercel.app",
+    siteName: "Malawian Writer",
+    type: "website",
+  },
 };
 
 const StoriesPage = () => {
-	const storyMetadata = getStoriesMetadata();
+  const storyMetadata = getStoriesMetadata();
 
-	return (
-		<PageWrapper>
-			<main className='mt-[12vh] py-16'>
-				<div className='w-full sm:w-10/12 max-w-4xl p-4 lg:p-0 mx-auto'>
-					<div className='mb-8 lg:mb-16'>
-						<h1 className='text-2xl font-black text-center py-4 font-Dancing-Script'>
-							On Stories<span className='text-brand'>.</span>
-						</h1>
-						<p className='font-medium text-center'>
-							<em>
-								&ldquo;Get an in-depth breakdown of my work and what I am currently working on. Bit
-								by bit.&rdquo;
-							</em>
-						</p>
-					</div>
-					<Stories storyMetadata={storyMetadata} />
-				</div>
-			</main>
-		</PageWrapper>
-	);
+  return (
+    <PageWrapper>
+      <main className="mt-[12vh] py-16">
+        <div className="w-full sm:w-10/12 max-w-4xl p-4 lg:p-0 mx-auto">
+          <div className="mb-8">
+            <h1 className="text-2xl font-black text-center py-4 font-Dancing-Script">
+              On Stories<span className="text-brand">.</span>
+            </h1>
+            <p className="font-medium text-center">
+              <em>
+                &ldquo;Get an in-depth breakdown of my work and what I am currently working on. Bit
+                by bit.&rdquo;
+              </em>
+            </p>
+          </div>
+          <Stories storyMetadata={storyMetadata} />
+        </div>
+      </main>
+    </PageWrapper>
+  );
 };
 
 export default StoriesPage;
--- components/Blog/PostCard.tsx
+++ components/Blog/PostCard.tsx
@@ -1,28 +1,31 @@
-import Link from 'next/link';
-import PostCardProps from '../models/postCard';
-import moment from 'moment';
-import 'moment-timezone';
-import Tags from '../Tags';
+import Link from "next/link";
+import PostCardProps from "../models/postCard";
+import moment from "moment";
+import "moment-timezone";
+import Tags from "../Tags";
 
 const PostCard = ({ post }: PostCardProps) => {
-	const { title, date, description, slug, tags } = post;
+  const { title, date, description, slug, tags } = post;
 
-	return (
-		<div className='border-b-[1px] last:border-none dark:border-gray-300 dark:border-opacity-20 py-8 md:pb-8 md:mb-0 hover:scale-95 transform transition-all ease-in-out duration-300'>
-			<small className='text-gray-700 dark:text-gray-300 opacity-80'>
-				{moment(date).format('dddd, Do MMMM YYYY')}
-			</small>
-			<Link href={`/blog/${slug}`}>
-				<h2 className='text-brand dark:hover:text-gray-300 text-2xl md:text-3xl 2xl:text-4xl font-serif font-bold'>
-					{title}
-				</h2>
-			</Link>
-			<p className='text-sm md:text-md 2xl:text-lg font-Baskerville text-zinc-800 mt-4'>
-				{description}
-			</p>
-			<Tags tags={tags} />
-		</div>
-	);
+  return (
+    <div className="border-b-[1px] last:border-none dark:border-gray-300 dark:border-opacity-20 py-8 md:pb-8 md:mb-0 hover:-translate-y-4 transform transition-all ease-in-out duration-300">
+      <small className="text-gray-700 dark:text-gray-300 opacity-80">
+        {moment(date).format("dddd, Do MMMM YYYY")}
+      </small>
+      <Link href={`/blog/${slug}`}>
+        <h2 className="text-brand dark:hover:text-gray-300 text-2xl md:text-3xl 2xl:text-4xl font-serif font-bold hover:underline">
+          {title}
+        </h2>
+      </Link>
+      <p className="text-sm md:text-md 2xl:text-lg font-Baskerville text-zinc-800 my-4">
+        {description}
+      </p>
+      <Link href={`/stories/${slug}`} className="text-brand hover:underline">
+        Read More...
+      </Link>
+      <Tags tags={tags} />
+    </div>
+  );
 };
 
 export default PostCard;
