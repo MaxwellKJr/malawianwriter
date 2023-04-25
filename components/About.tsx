b04467f3e8d7db2dd414e2d92904c6631a5eea59
maxwellkjr <maxwellkjr@gmail.com>
2023-04-25 21:58:10 +0200

    Created about Component

---
+++ components/About.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2 className="text-lg font-bold">About Me</h2>
+      <p>I am writer just trying to perfect my craft with each passing day.</p>
+    </div>
+  );
+}
+
+export default About;
--- components/Showcase.tsx
+++ components/Showcase.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React from "react";
+import About from "./About";
 
 const Showcase = () => {
   return (
-    <section className='flex flex-col w-full h-[90vh] justify-center items-center mx-auto px-4 bg-zigzag bg-repeat bg-cover bg-center'>
-      <div className='text-left w-full sm:w-10/12 max-w-4xl mx-auto'>
-        <h1 className='text-3xl md:text-4xl lg:text-5xl font-black dark:text-white font-Poppins'>
-          A writer, <span>fiending <br className='hidden lg:block' />
-            for <span className='text-brand'>great</span> stories</span>
-          <span className='text-brand'>.</span></h1>
-        <p className='font-Baskerville text-sm sm:text-md text-slate-800 mt-6 lg:max-w-md'>
-          It all started with <a href='https://amazon.com/Harry-Potter-Sorcerers-Stone-Rowling/dp/059035342X' target='_blank' rel='noopener noreferrer' className='text-brand dark:text-brand hover:underline'><em>Harry Potter and The Sorcerer&apos;s Stone</em></a> and the rest is, well... history.
+    <section className="flex flex-col w-full h-[90vh] justify-center items-center mx-auto px-4 bg-zigzag bg-repeat bg-cover bg-center">
+      <div className="text-left w-full sm:w-10/12 max-w-4xl mx-auto">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-black dark:text-white font-Poppins">
+          A writer,{" "}
+          <span>
+            fiending <br className="hidden lg:block" />
+            for <span className="text-brand">great</span> stories
+          </span>
+          <span className="text-brand">.</span>
+        </h1>
+        <p className="font-Baskerville text-sm sm:text-md text-slate-800 mt-6 lg:max-w-md">
+          It all started with{" "}
+          <a
+            href="https://amazon.com/Harry-Potter-Sorcerers-Stone-Rowling/dp/059035342X"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-brand dark:text-brand hover:underline"
+          >
+            <em>Harry Potter and The Sorcerer&apos;s Stone</em>
+          </a>{" "}
+          and the rest is, well... history.
         </p>
       </div>
+      <About />
     </section>
   );
 };
