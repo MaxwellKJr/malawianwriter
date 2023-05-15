820a8f8b9749bb8362bb7957dbea7cd00e9fbb0b
maxwellkjr <maxwellkjr@gmail.com>
2023-05-15 23:49:29 +0200

    Added content to the about description

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div>
       <h2 className="text-lg font-bold">About Me</h2>
-      <p>I am writer just trying to perfect my craft with each passing day.</p>
+      <p>I am writer just trying to perfect my craft with each passing day. I am a huge fan of fantasy novels but I generally read anything, as long as it interests me.</p>
     </div>
   );
 }
